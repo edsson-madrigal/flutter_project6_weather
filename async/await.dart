214713790d7ee53d872a94438400efe2a13702d2
Edsson <akairu@hotmail.com>
2022-07-24 15:43:28 -0500

    async await

---
+++ async/await.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: unused_local_variable, avoid_print, prefer_const_constructors
+
+import 'dart:io';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() {
+  task1();
+  task2();
+  task3();
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+void task2() {
+  //Duration pause = Duration(seconds: 3);
+  //sleep(pause);
+  String result = 'task 2 data';
+  print('Task 2 complete');
+}
+
+void task3() {
+  String result = 'task 3 data';
+  print('Task 3 complete');
+}
